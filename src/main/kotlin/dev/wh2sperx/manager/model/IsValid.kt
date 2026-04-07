50b028478215d193594bef1515131af2ff34b8e0
wh2sperx <wh2sperx@gmail.com>
2026-04-07 16:30:44 +0700

    Update .gitignore and remove untracked files

--- src/main/kotlin/dev/wh2sperx/manager/model/IsValid.kt
+++
@@ -1,8 +0,0 @@
-package dev.wh2sperx.manager.model
-
-import net.luckperms.api.model.user.User
-
-data class IsValid(
-    val valid: Boolean,
-    val user: User? = null
-)
--- src/main/kotlin/dev/wh2sperx/storage/model/PlayerData.kt
+++
@@ -1,9 +0,0 @@
-package dev.wh2sperx.storage.model
-
-import java.util.*
-
-data class PlayerData(
-    val uuid: UUID,
-    val password: String,
-    val issuedAt: Long
-)
